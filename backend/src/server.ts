347a3923044ed6cb5a17874533cbb30d8d1780d2
Cabdalle Moha <abdallamoha243@gmail.com>
2026-08-11 01:55:28 -0700

    chore: fix backend repository structure

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import app from "./app";
+import dotenv from "dotenv";
+import connectDB from "./config/dbs";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+connectDB()
+app.listen(PORT ,() =>{
+    console.log(`Server is running on port http://localhost:${PORT}`);
+})
+
